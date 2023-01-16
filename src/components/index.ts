f8f11c741b1f5839231cd6532381d1e2ca0a4466
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-17 02:45:16 +0800

    Barrel component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 import Button from './Button'
+import Input from './Input'
+import Text from './Text'
+import FormInput from './FormInput'
 
-export { Button }
+export { Button, Input, Text, FormInput }
